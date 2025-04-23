a38a6250cb12b177b87a1f88869c29e35d414263
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-04-23 14:51:15 +0200

    comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Khrona | The iOS App",
   description: "Get Your Life Together",
+  keywords: "life tracking app, goal tracking, daily journal, mood tracker, self-awareness app, iOS app, personal development",
+  openGraph: {
+    title: "Khrona | Track Your Life & Daily Goals",
+    description: "Track your days, mood, goals, and reflections — all in one place. Gain clarity, one day at a time.",
+    type: "website",
+    url: "https://khrona-website.vercel.app/",
+    images: [
+      {
+        url: "https://khrona-website.vercel.app/k3dblack.png",
+        width: 1200,
+        height: 630,
+        alt: "Khrona App Preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
